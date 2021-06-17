9984f7e34a9b3463a8c7735cdd4533a13185eaaa
FLAVIO CELESTINO <81762067+punk23@users.noreply.github.com>
2021-06-16 21:51:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reajuste_Salarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Informe o valor do salário: ");
+            string salario = Console.ReadLine();
+            double valorSalario = Convert.ToDouble(salario);
+            Console.WriteLine("Informe a porcentagem de reajuste: ");
+            string reajuste = Console.ReadLine();
+            double valorReajuste = Convert.ToDouble(reajuste);
+            double salarioAjustado;
+            salarioAjustado = valorSalario+(valorSalario * (valorReajuste / 100));
+
+            Console.WriteLine("O valor do seu salário reajustado é: R$ " + salarioAjustado);
+
+        }
+    }
+}
